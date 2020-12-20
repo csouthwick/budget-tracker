06c026d89ce80358f6fb4b6ab4c94c2726f0a543
Chris Southwick <csouthwick@live.com>
2020-12-20 17:11:48 -0500

    upload pending transactions when connection to server is restored

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,9 +13,9 @@ request.onsuccess = function (e) {
   db = e.target.result;
 
   // if online, upload any pending records
-  // if (navigator.onLine) {
-  //   uploadRecords();
-  // }
+  if (navigator.onLine) {
+    uploadRecords();
+  }
 };
 
 request.onerror = function (event) {
@@ -30,3 +30,40 @@ function saveRecord(record) {
   const records = transaction.objectStore('records');
   records.add(record);
 }
+
+function uploadRecords() {
+  const transaction = db.transaction(['records'], 'readwrite');
+  const records = transaction.objectStore('records');
+  const getAll = records.getAll();
+
+  getAll.onsuccess = function () {
+    // If there are pending budget transactions, send to the server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          // if saving to server worked, clear pending records
+          const transaction = db.transaction(['records'], 'readwrite');
+          const records = transaction.objectStore('records');
+          records.clear();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+};
+
+// listen for online status
+window.addEventListener('online', uploadRecords);
